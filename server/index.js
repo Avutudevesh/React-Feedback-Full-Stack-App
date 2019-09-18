c328954d2e47ff9ddcb530607be9585665a77697
Devesh Avutu <deveshavutu@Deveshs-MacBook-Air.local>
2019-09-18 23:42:42 +0530

    testing redeployment on heroku

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) =>{res.send('Hi Hello There')});
+app.get('/', (req, res) =>{res.send('Testing redeployment')});
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
